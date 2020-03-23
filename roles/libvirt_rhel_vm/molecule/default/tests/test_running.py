92dc0ed94ffc594b08063764b6d21cfa2e3c3ba8
Gregory Hellings <greg.hellings@gmail.com>
2020-03-23 14:15:50 -0500

    Try without sudo?

--- roles/libvirt_rhel_vm/molecule/default/tests/test_running.py
+++ roles/libvirt_rhel_vm/molecule/default/tests/test_running.py
@@ -1,5 +1,4 @@
 def test_running(host):
     host = host.get_host('local://')  # Force to connect to localhost
-    with host.sudo():
-        virsh = host.check_output("virsh list")
+    virsh = host.check_output("virsh list")
     assert "foo.example.com" in virsh
