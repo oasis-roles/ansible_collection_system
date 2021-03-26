4ee710285fe44c8301dc0a817674e4907d045084
Miah Johnson <miajohns@redhat.com>
2021-03-26 13:00:21 -0400

    echo debug

--- .buildkite/verify.sh
+++ .buildkite/verify.sh
@@ -2,6 +2,14 @@
 
 set -euo pipefail
 
+echo "--- starting tests.."
+
 cd ../ansible_collections/oasis_roles/system
+
+echo "--- here we are:"
+
+pwd
+
+echo "+++ :one-does-not-simply: lets go!"
 
 tox -e roles-hostname-default
