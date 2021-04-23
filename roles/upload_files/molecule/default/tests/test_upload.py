1c638d15b6cc71b95670cf84134609a381879321
Gregory Hellings <greg.hellings@gmail.com>
2021-04-23 13:32:25 -0500

    Make directory paths

--- roles/upload_files/molecule/default/tests/test_upload.py
+++ roles/upload_files/molecule/default/tests/test_upload.py
@@ -5,6 +5,12 @@ def test_upload_settings(host):
     assert derp.group == 'wheel'
 
 
+def test_root_not_changed(host):
+    root = host.file('/')
+    assert root.group == 'root'
+    assert root.user == 'root'
+
+
 def test_upload_minimal(host):
     authors = host.file('/main.yml')
     assert authors.contains('-')
